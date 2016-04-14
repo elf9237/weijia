24f3de84485052c8b5daf35425ad03c0d69c846c
inspurhua <aczhanghua@gmail.com>
2016-04-14 10:38:33 +0800

    userinfo

--- app/index/protected/controllers/SiteController.php
+++ app/index/protected/controllers/SiteController.php
@@ -114,8 +114,12 @@ class SiteController extends BaseController
 	}
 	public function actionJoin(){
 		$model=new Agentform();
+		$user_info =  Yii::app()->user->userdetail;
+		$model->cellphone = $user_info->cellphone;
 		if(isset($_POST['Agentform'])){
 			$model->attributes=$_POST['Agentform'];
+			$model->user_id = $user_info->id;
+			$model->user_name = $user_info->user_name;
 
 			if($model->save()){
 				$this->redirect('index.php?r=site/joinsuccess');
