a813471988f86023b412790eb4f280a1db7e41da
inspurhua <aczhanghua@gmail.com>
2016-04-15 12:00:09 +0800

    1

---
+++ app/index/protected/views/site/kangfangsave.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <title>微家</title>
+    <link rel="stylesheet" href="css/frozen.css">
+    <link href="./public/css/iSlider.css" rel="stylesheet">
+    <link href="css/house.css" rel="stylesheet">
+    <style>
+        /*ul wrapper*/
+        #iSlider-wrapper {
+            height: 90%;
+            width: 100%;
+            overflow: hidden;
+            position: absolute;
+        }
+        #iSlider-wrapper ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden;
+        }
+
+        #iSlider-wrapper li {
+            position: absolute;
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden;
+            display: -webkit-box;
+            -webkit-box-pack: center;
+            -webkit-box-align: center;
+            list-style: none;
+        }
+
+        #iSlider-wrapper li img {
+            max-width: 100%;
+            max-height: 100%;
+        }
+
+        .islider-btn-outer {
+            background-color: rgba(0, 0, 0, .5);
+            border-radius: 99px;
+        }
+
+        .islider-btn-inner {
+            height: 30%;
+            width: 30%;
+            margin-top: 34%;
+        }
+    </style>
+</head>
+<body ontouchstart="">
+
+<section class="ui-notice">
+    <i></i>
+    <p>信息已提交</p>
+    <div class="ui-notice-btn">
+        <button class="ui-btn-primary ui-btn-lg">返回</button>
+    </div>
+</section>
+<script src="lib/zepto.min.js"></script>
+<script src="js/frozen.js"></script>
+<script src="js/house.js"></script>
+</body>
+</html>
