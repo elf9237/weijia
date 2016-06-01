130d3c3a1f4df092a2183091678b6d8c3618b29a
fanyy <1102716160@qq.com>
2016-06-01 23:23:21 +0800

    手机端查询页面

--- app/index/lib/searchDiv.js
+++ app/index/lib/searchDiv.js
@@ -1,4 +1,4 @@
-﻿ function popCen(th,type){
+function popCen(th,type){
 	var cla='cla'+type;
 	if($(".filter-wrap ."+cla).length>0){
 	$(".filter-wrap .dqld_div").remove();
@@ -8,7 +8,7 @@
 	var province=[];
 	var zujin=['1000以下','1000-1500','1500-2000','2000-2500','2500-3000','3000-3500','3500以上'];
 	var huxin=['单间','2室','3室','4室'];
-	var laiyuan=['个人','平台','加盟'];
+	var laiyuan=['月租','日租','商铺'];
 	
 	if(type==1){
 		province=zujin;
@@ -25,16 +25,81 @@
 	newStr.push("<div class='"+realClass+" ' ><ul>");
 	for(var i=0,psize=province.length;i<psize;i++){
 		
-		newStr.push("<li onclick=\"getRealValue('"+id+"',this)\">"+province[i]+"</li>");
+		newStr.push("<li onclick=\"getRealValue('"+id+"',this,"+type+")\">"+province[i]+"</li>");
 	}
 	newStr.push("</ul></div>");
 	$(".filter-wrap").append(newStr.join(""));
 	
 	
 }
-function getRealValue(thf,th){
+function getRealValue(thf,th,type){
 	$("#"+thf).text($(th).text());
+        if(type==1){
+            if($(th).text()=="1000以下"){
+                param.sprice=0;
+                  param.eprice=1000;
+            }
+             if($(th).text()=="1000-1500"){
+                param.sprice=1000;
+                  param.eprice=1500;
+            }
+             if($(th).text()=="1500-2000"){
+                param.sprice=1500;
+                  param.eprice=2000;
+            }
+             if($(th).text()=="2000-2500"){
+                param.sprice=2000;
+                  param.eprice=2500;
+            }
+              if($(th).text()=="2500-3000"){
+                param.sprice=2500;
+                  param.eprice=3000;
+            }
+              if($(th).text()=="3000-3500"){
+                param.sprice=3000;
+                  param.eprice=3500;
+            }
+            if($(th).text()=="3500以上"){
+                param.sprice=3500;
+                  param.eprice=15000;
+            }
+        }
+        if(type==2){
+              if($(th).text()=="单间"){
+                param.rooms=1;
+                 
+            }
+                 if($(th).text()=="2室"){
+                param.rooms=2;
+                
+            }
+                 if($(th).text()=="3室"){
+               param.rooms=3;
+            }
+                 if($(th).text()=="4室"){
+                param.rooms=4;
+                 
+            }
+            
+        }
+        if(type==3){
+              if($(th).text()=="月租"){
+                param.info_type=0;
+                 
+            }
+                 if($(th).text()=="日租"){
+                param.info_type=1;
+                
+            }
+                 if($(th).text()=="商铺"){
+               param.info_type=2;
+            }
+            
+        }
+        param.page=0;
+        param.totalPage=1;
 	$(".filter-wrap .dqld_div").remove();
+        querySecond();
 	
 	
 }
--- app/index/protected/views/store/shopstore.php
+++ app/index/protected/views/store/shopstore.php
@@ -33,9 +33,9 @@
                             <div class="address_input"><input class="address_input1" type="text" placeholder="省市区" id="shengshi" onClick="getProvinceBuy()" readonly="readonly"></div>
                         </div>
                     </a></li>
-                    <li class="nav-name icon down"><a href="javascript:;">租金</a></li>
-                    <li class="nav-name icon down"><a href="javascript:;">户型室</a></li>
-                    <li class="nav-name icon down"><a href="javascript:;">个人</a></li>
+                    <li class="nav-name icon down"><a href="javascript:;" id='zj' onclick="popCen(this,1)">租金</a></li>
+                    <li class="nav-name icon down"><a href="javascript:;" id='hx' onclick="popCen(this,2)">户型室</a></li>
+                    <li class="nav-name icon down"><a href="javascript:;" id='ly' onclick="popCen(this,3)">类别</a></li>
                     </ul>
                     </div>
                 </div>
@@ -51,7 +51,7 @@
         <script src="lib/zepto.min.js"></script>
          <script src="lib/dropload.min.js"></script>
         <script src="js/frozen.js"></script>
-        
+        <script src="lib/searchDiv.js"></script>
         <script src="js/house.js"></script>
         <script>
             function getLocalTime(nS) {     
@@ -109,7 +109,7 @@
                          $.each(data.pageList,function(n,value){
                              
                                  innerHtml.push('<li class="item">'+
-                        '<a onclick="showRoom('+value.id+')>'+
+                        '<a onclick=" ('+value.id+')>'+
                             '<img src="upload/'+value.mian_url+'" alt="" class="item-thumb">'+
                             '<dl class="item-info">'+
                                ' <dt class="info-title"><strong>'+value.info_name+'</strong></dt>'+
@@ -138,6 +138,50 @@
             }
         }).scroll();
     })}
+    function querySecond(){
+    
+     $.ajax({
+                    type: 'POST',
+                    url: "index.php?r=ajax/queryFangyu",
+               data:param,
+                    dataType: 'json',
+                    success: function(data){
+                        param.totalPage=data.totaPage;//更新总页数
+                        param.page=data.currentPage;//更新当前页（js不太可靠）
+                         var innerHtml=[];
+                         $.each(data.pageList,function(n,value){
+                             
+                                 innerHtml.push('<li class="item">'+
+                        '<a onclick="showRoom('+value.id+')>'+
+                            '<img src="upload/'+value.mian_url+'" alt="" class="item-thumb">'+
+                            '<dl class="item-info">'+
+                               ' <dt class="info-title"><strong>'+value.info_name+'</strong></dt>'+
+                               ' <dd class="info-desc">'+
+                                    '<span class="info-desc-detail"><em>'+value.house_type+'</em>'+value.city+'-'+value.zone+'-'+value.district+' </span>'+
+                                    '<span class="info-desc-tag info-desc-tag--right"> <em class="biz">置顶</em> </span>'+
+                                '</dd>'+
+                                
+                                '<dd class="info-desc">'+
+                                '<span class="info-desc-price"> '+value.price+'<em class="priceunit"> 元</em></span><span class="info-desc-tag"><em class="time">'+getLocalTime(value.create_time)+'</em></span><span class="info-desc-tag--right">  <em class="personal">个人</em>    </span>'+
+                                '</dd></dl> </a>  </li>');
+                            
+                       
+                  
+                             
+                         
+    });
+    $(".infoList").html(innerHtml.join(""));
+                        $(".loading").remove();//删除加载图片
+                        ajax=!1;//注明已经完成ajax加载
+                    },
+                    error: function(xhr, type){
+                        $(".loading").html("暂无内容！");
+                    }
+                });
+    }
+    function showRoom(){
+    
+    }
     
     
    
